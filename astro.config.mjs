3a7aec86cad137ae210ab646044481bafceaa1a0
jlewisbooth <jlewisbooth@gmail.com>
2025-01-21 13:45:56 +0000

    base updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jlewisbooth.github.io",
-  base: "",
-  integrations: [tailwind()]
+  base: "adrian-sutton",
+  integrations: [tailwind()],
 });
